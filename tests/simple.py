5d4a25b1cd227596f1e02421d69a07ec481e486e
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-12-05 14:06:42 +0530

    Added basic test case

--- tests/simple.py
+++ tests/simple.py
@@ -1,20 +1,50 @@
-Test Recipe.
+# simple execution of each instruction
+
+import sys
+sys.path.append('/home/nikhil/workspace/preserve')
+
+from cStringIO import StringIO
+from preserve import run
+
+recipe = """
+Simple.
 
 Ingredients.
-72 g haricot beans
-101 eggs
-108 g lard
-111 cups oil
-32 zucchinis
-119 ml water
-114 g red salmon
-100 g dijon mustard
-33 potatoes
+2 beans
 
-Cooking time: 4.
-Pre-heat oven to 45 degrees Celcius.
-Cooking time: 5 minute.
-Cooking time: 5 hour.
-Cooking time: 5 hours.
-Cooking time: 5 minutes.
-Cooking time: 5 duff.
+Method.
+Take apricot jam from refrigerator.
+Put apricot jam into the mixing bowl.
+Add apricot jam to the mixing bowl.
+Add apricot jam to the mixing bowl.
+Pour contents of the mixing bowl into the baking dish.
+Fold apricot jam into mixing bowl.
+Put beans into 2nd mixing bowl.
+Add beans to 2nd mixing bowl.
+Remove beans from 2nd mixing bowl.
+Serves 1.
+
+"""
+
+stdin = sys.stdin
+stdout = sys.stdout
+
+sys.stdin = StringIO()
+sys.stdout = StringIO()
+
+sys.stdin.write('1\n')
+sys.stdin.seek(0)
+
+result = run(recipe)
+sys.stdin.close()
+
+sys.stdout.seek(0)
+out = sys.stdout.read()
+
+sys.stdin = stdin
+sys.stdout = stdout
+
+print result
+assert result
+assert len(result.ingredients) == 2
+assert result.ingredients['apricot jam'].value == 3
