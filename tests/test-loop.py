1438a998c73b7fe100fc683d3bad149ce04d4639
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-12-11 20:08:51 +0530

    Added more loop tests

--- tests/test-loop.py
+++ tests/test-loop.py
@@ -3,7 +3,7 @@ sys.path.append('/home/nikhil/workspace/preserve')
 
 from nose.tools import assert_raises
 
-from preserve import run
+from preserve import run, interpret, parse
 
 recipe_simple_loop = """
 Fruity Loops 1.
@@ -45,6 +45,60 @@ nesting = run(recipe_nesting)
 assert nesting.ingredients['flour'].value == 0
 assert nesting.ingredients['cheese'].value == 0
 assert len(nesting.mixing_bowls[1]) == 15
+
+recipe_err_nesting = """
+Fruity Loops Broken Nesting.
+
+Ingredients.
+
+Method.
+Sift the flour.
+  Sift the flour.
+    Put honey into mixing bowl.
+Shake the flour until sifted.
+"""
+
+assert_raises(SyntaxError, run, recipe_err_nesting)
+
+recipe_verb_match = """
+Fruity Loops Verb Match.
+
+Ingredients.
+1 g flour
+1 g cheese
+
+Method.
+Sift the flour.
+  Grate the cheese.
+    Gyrate the hips.
+    Dance until gyrateed.
+  Grate the cheese until grateed.
+Shake the flour until sifted.
+"""
+
+verb_match_ast = parse(recipe_verb_match)
+verb_match_i = interpret(verb_match_ast[0], verb_match_ast)
+
+verb_match = verb_match_i.cook()
+assert len(verb_match.ingredients) == 3
+
+recipe_verb_match_fail = """
+Fruity Loops Verb Match Fail.
+
+Ingredients.
+1 g flour
+1 g cheese
+
+Method.
+Sift the flour.
+  Grate the cheese.
+    Gyrate the hips.
+    Dance until gyrate.
+  Grate the cheese until grateed.
+Shake the flour until sifted.
+"""
+
+assert_raises(NameError, run, recipe_verb_match_fail)
 
 recipe_err_nesting = """
 Fruity Loops Broken Nesting.
